6de078ba0360e134aed8447c69c4cd1c102d0c7b
DongGyu Won <wdg980813@gmail.com>
2021-08-31 23:11:27 +0900

    5th

--- practice/src/App.js
+++ practice/src/App.js
@@ -10,6 +10,7 @@ function App() {
 
   let [추천, 추천변경] = useState(['소고기','생선','야채']);// 자주 변하는 중요한 데이터들을 새로고침 없이 재랜더링 가능케하는 State
   let [goodNum, goodNum_C] = useState(0);
+  let [onoff, onoff_C] = useState(false);
 
   function changetit(){
     let newArray = [...추천];
@@ -35,11 +36,35 @@ function App() {
           <hr/>
         </div>
         <div className="list">
-          <h2>오늘은 {추천[2]}</h2>
+          <h2>
+            오늘은 {추천[2]}<button className="modal_Btn" onClick={() => {onoff_C(!onoff)}}></button> 
+          </h2>
           <p>날짜: 2021.08.30</p>
           <hr/>
         </div>
       </main>
+
+      {
+        onoff === true
+        ? <Modal></Modal>
+        : null
+      }
+
+    </div>
+  );
+}
+
+// component로 만들면 좋은 것은 반복 출현 하는 html / 자주변경되는 html UI 들
+// 단점 -state 쓸 때 복잡
+function Modal(){//component 함수 이름은 항상 대문자/ 축약해서 넣을 수 있음 
+  return(// return 안에 div(body 같은 느낌)는 하나만 가능 
+    <div className="modal">
+      <div className="modal_in">
+        <button className="modal_close">X</button>
+        <h2>광고</h2>
+        <p>오세요 오세요~</p>
+        <p className="today">날짜: 21.08.31</p>
+      </div>
     </div>
   );
 }
