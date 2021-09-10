a11ea5182f10d792784f75dcd1b7aa4644a7d101
DongGyu Won <wdg980813@gmail.com>
2021-09-11 02:51:04 +0900

    12th

--- mall/src/App.js
+++ mall/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import { Navbar, Container,NavDropdown,Nav,Button } from "react-bootstrap";
 import React, { useState } from 'react';
+import data from './data.js';
 
 function App() {
 
-  let [shoes, shoes_c] = useState([]);
+  let [shoes, shoes_c] = useState(data);
   // export default 함수명 = 내보내기 받을 때는 import 임의명 from './경로'
   // export 보통 페이지 마지막에 옴, 페이지당 한번 밖에 쓸 수 없음
 
@@ -22,7 +23,7 @@ function App() {
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
+                <NavDropdown.Divider/>
                 <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
               </NavDropdown>
             </Nav>
@@ -38,23 +39,26 @@ function App() {
 
       <div className="container">
         <div className="row">
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" alt="" />
-            <h4>운동화</h4>
-            <p>이것은 운동화/가격 : 89000원</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes2.jpg" alt="" />
-            <h4>운동화</h4>
-            <p>이것은 운동화/가격 : 89000원</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes3.jpg" alt="" />
-            <h4>운동화</h4>
-            <p>이것은 운동화/가격 : 89000원</p>
-          </div>
+          {
+            shoes.map((re, i) => {
+              return(
+                <Contents key={i} shoes={shoes[i]}/>
+              );
+            })
+          }
+
         </div>
       </div>
+    </div>
+  );
+}
+
+function Contents(props){
+  return(
+    <div className="col-md-4">
+      <img src={'https://codingapple1.github.io/shop/shoes' + props.shoes.id + '.jpg'} alt="" />
+      <h4>{props.shoes.title}</h4>
+      <p>가격 : {props.shoes.price}원</p>
     </div>
   );
 }
